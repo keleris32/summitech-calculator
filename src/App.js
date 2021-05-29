503deb409fe76938ac7d68ac2f0098e0d030bcc0
keleris32 <charles.ejiegbu@gmail.com>
2021-05-29 02:51:20 +0100

    prototype

--- src/App.js
+++ src/App.js
@@ -1,32 +1,76 @@
 import './App.css';
+import Button from './Component/Button';
 import { useOperations } from './useOperations';
 
 function App() {
-  const [result, handleClick, clearResult] = useOperations();
+  // Custom hook to abstract calculator logic
+  const [
+    input,
+    disableBtn,
+    handleDigit,
+    handleOperator,
+    clearResult,
+    calculateResult,
+  ] = useOperations();
 
   return (
     <div className="App">
       <div className="calculator">
-        <form className="output">
-          <input type="text" value={result} />
-        </form>
+        <div className="output">{input}</div>
         <div className="key-pad">
-          <button name="7" onClick={handleClick} className="digit-7">7</button>
-          <button name="8" onClick={handleClick} className="digit-8">8</button>
-          <button name="9" onClick={handleClick} className="digit-9">9</button>
-          <button name="/" onClick={handleClick} className="op-div">&divide;</button>
-          <button name="4" onClick={handleClick} className="digit-4">4</button>
-          <button name="5" onClick={handleClick} className="digit-5">5</button>
-          <button name="6" onClick={handleClick} className="digit-6">6</button>
-          <button name="*" onClick={handleClick} className="op-mul">&times;</button>
-          <button name="1" onClick={handleClick} className="digit-1">1</button>
-          <button name="2" onClick={handleClick} className="digit-2">2</button>
-          <button name="3" onClick={handleClick} className="digit-3">3</button>
-          <button name="-" onClick={handleClick} className="op-sub">&ndash;</button>
-          <button name="0" onClick={handleClick} className="digit-0">0</button>
-          <button onClick={clearResult} className="clear">C</button>
-          <button onClick={handleClick} className="eq">=</button>
-          <button name="+" onClick={handleClick} className="op-add">+</button>
+          {/* <Button value="7" /> */}
+          <button onClick={handleDigit}>7</button>
+          <button onClick={handleDigit}>8</button>
+          <button onClick={handleDigit}>9</button>
+          {disableBtn ? (
+            <button disabled={true} onClick={handleOperator} className="op-btn">
+              /
+            </button>
+          ) : (
+            <button onClick={handleOperator} className="op-btn">
+              /
+            </button>
+          )}
+          <button onClick={handleDigit}>4</button>
+          <button onClick={handleDigit}>5</button>
+          <button onClick={handleDigit}>6</button>
+          {disableBtn ? (
+            <button disabled={true} onClick={handleOperator} className="op-btn">
+              x
+            </button>
+          ) : (
+            <button onClick={handleOperator} className="op-btn">
+              x
+            </button>
+          )}
+          <button onClick={handleDigit}>1</button>
+          <button onClick={handleDigit}>2</button>
+          <button onClick={handleDigit}>3</button>
+          {disableBtn ? (
+            <button disabled={true} onClick={handleOperator} className="op-btn">
+              -
+            </button>
+          ) : (
+            <button onClick={handleOperator} className="op-btn">
+              -
+            </button>
+          )}
+          <button onClick={handleDigit}>0</button>
+          <button onClick={clearResult} className="sp-btn">
+            C
+          </button>
+          <button onClick={calculateResult} className="sp-btn">
+            =
+          </button>
+          {disableBtn ? (
+            <button disabled={true} onClick={handleOperator} className="op-btn">
+              +
+            </button>
+          ) : (
+            <button onClick={handleOperator} className="op-btn">
+              +
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/useOperations.js
+++ src/useOperations.js
@@ -1,15 +1,117 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useOperations = () => {
-  const [result, setResult] = useState('');
+  const [input, setInput] = useState('');
+  const [previousState, setPreviousState] = useState('');
+  const [currentState, setCurrentState] = useState('');
+  const [operator, setOperator] = useState(null);
+  const [disableBtn, setDisableBtn] = useState(false);
+  // const [result, setResult] = useState(false);
 
-  const handleClick = (e) => {
-    setResult(result.concat(e.target.name));
+  //  To handle logic after button press
+  const handleDigit = (e) => {
+    const digit = e.target.innerText;
+
+    // If the previousState is empty and the digit clicked is "0", do nothing.
+    if (input === '' && digit === '0') return;
+
+    // If input is empty, append the value of each digit to the input;
+    if (input === '') {
+      setCurrentState(digit);
+      setInput(digit);
+    } else {
+      setCurrentState((prevInput) => prevInput + digit);
+      setInput((prevInput) => prevInput + digit);
+    }
+
+    if (input.length > 10) return;
+  };
+
+  const handleOperator = (e) => {
+    const operatorDigit = e.target.innerText;
+
+    // If the display is empty and value of the Button press = "-", set the display to operatorAigit
+    if (input === '' && operatorDigit === '-') {
+      setInput(operatorDigit);
+    } else return;
+
+    if (currentState !== '' && operator !== '') {
+      setOperator(operatorDigit);
+      setInput(currentState + operatorDigit);
+      setPreviousState(currentState);
+      setCurrentState('');
+    } else {
+      setInput(previousState + operatorDigit);
+      setOperator(operatorDigit);
+    }
+
+    if (currentState === '' && operatorDigit === '-' && operator !== '-') {
+      setInput(previousState + operator + operatorDigit);
+      setCurrentState(operatorDigit);
+      setDisableBtn(true);
+    } else return;
+
+    if (input === '' && operatorDigit === '-') {
+      setInput(operatorDigit);
+    } else return;
   };
 
   const clearResult = () => {
-    setResult('');
+    setInput('');
+    setPreviousState('');
+    setCurrentState('');
+    setOperator(null);
+    setDisableBtn(false);
+
+    console.log(currentState);
   };
 
-  return [result, handleClick, clearResult];
+  const calculateResult = () => {
+    // setResult(eval(result).toString());
+    console.log(`PreviousState = ${previousState}`);
+    console.log(`Operator = ${operator}`);
+    console.log(`currentState = ${currentState}`);
+    console.log(`disableBtn = ${disableBtn}`);
+
+    if (previousState !== '' && operator !== '' && currentState !== '') {
+      let calculate;
+      switch (operator) {
+        case '/':
+          calculate = String(
+            Math.floor(parseInt(previousState) / parseInt(currentState))
+          );
+          break;
+
+        case '+':
+          calculate = String(parseInt(previousState) + parseInt(currentState));
+          break;
+
+        case 'x':
+          calculate = String(parseInt(previousState) * parseInt(currentState));
+          break;
+
+        case '-':
+          calculate = String(parseInt(previousState) - parseInt(currentState));
+          break;
+
+        default:
+          return;
+      }
+
+      setInput(calculate);
+      setPreviousState('');
+      setCurrentState('');
+      setOperator(null);
+      setDisableBtn(true);
+    }
+  };
+
+  return [
+    input,
+    disableBtn,
+    handleDigit,
+    handleOperator,
+    clearResult,
+    calculateResult,
+  ];
 };
